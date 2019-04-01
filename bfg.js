3a7b48225ae2a9f74494b7ef4e3ddfb563b7355a
amrwski <cmf2071@gmail.com>
2019-04-01 19:15:15 +0200

    start work on shooting mechanic

--- bfg.js
+++ bfg.js
@@ -1,19 +1,27 @@
-function BFG() {
-  this.pos = createVector()
-  this.vel = createVector()
+function BFG(ghostPos, angle) {
+  this.pos = createVector(ghostPos.x, ghostPos.y)
+  this.vel = createVector() //////
 
   this.update = function() {
     this.pos.add(this.vel)
   }
 
   this.render = function() {
+    push()
     stroke(255)
-    strokeWeight(3)
-    PointerEvent(this.x, this.y)
+    strokeWeight(0.5)
+    line(ghost.pos.x, ghost.pos.y, mouseX, mouseY)
+    pop()
   }
 }
 
-this.shooting = function() {
+function fire() {
+  if (mouseIsPressed) {
+    slugs.push(new BFG(ghost.pos, (mouseX, mouseY)))
+  }
+}
+
+this.printXY = function() {
   if (mouseIsPressed) {
     console.log(mouseX, mouseY)
   }
--- game.js
+++ game.js
@@ -11,7 +11,13 @@ function draw() {
   background(0)
   frameRate(60)
   ghost.render()
-  this.shooting()
+  this.fire()
+  this.printXY()
+
+  for (let i = 0; i < slugs.length; i++) {
+    slugs[i].render()
+    slugs[i].update()
+  }
 
   text("X: " + mouseX, 0, HEIGHT / 4)
   text("Y: " + mouseY, 0, HEIGHT / 2)
