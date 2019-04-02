d32e6ab6a1286554ffaf2321f4b2a4b315030ec4
amrwski <cmf2071@gmail.com>
2019-04-02 12:49:20 +0200

    working shooting mechanic

--- bfg.js
+++ bfg.js
@@ -1,4 +1,4 @@
-class BFG {
+class Bullet {
   constructor(mouseX, mouseY, ghostX, ghostY) {
     this.ghostX = ghostX
     this.ghostY = ghostY
@@ -11,53 +11,17 @@ class BFG {
     push()
     stroke(255)
     strokeWeight(0.5)
-    /* line(this.pos.x,this.pos.y,mouseX,mouseY) */
-    line(this.mouseX, this.mouseY, this.pos.x, this.pos.y)
+    fill(200, 0, 200)
+    ellipse(this.ghostX, this.ghostY, 5)
+    this.ghostX += (this.mouseX - ghost.pos.x) * 0.1
+    this.ghostY += (this.mouseY - ghost.pos.y) * 0.1
     pop()
   }
   update() {
     this.pos.add(this.vel)
-    /* this.pos.x-=1 */
   }
 }
 
-function fire() {
-  if (mouseIsPressed) {
-    slugs.push(new BFG(mouseX, mouseY, ghost.pos.x, ghost.pos.y))
-  }
+function mouseClicked() {
+  slugs.push(new Bullet(this.mouseX, this.mouseY, ghost.pos.x, ghost.pos.y))
 }
-
-this.printXY = function() {
-  if (mouseIsPressed) {
-    console.log(mouseX, mouseY)
-  }
-}
-
-// function BFG(ghostPos, angle) {
-//   this.pos = createVector(ghostPos.x, ghostPos.y)
-//   this.vel = createVector() //////
-
-//   this.update = function() {
-//     this.pos.add(this.vel)
-//   }
-
-//   this.render = function() {
-//     push()
-//     stroke(255)
-//     strokeWeight(0.5)
-//     line(ghost.pos.x, ghost.pos.y, mouseX, mouseY)
-//     pop()
-//   }
-// }
-
-// function fire() {
-//   if (mouseIsPressed) {
-//     slugs.push(new BFG(ghost.pos, (mouseX, mouseY)))
-//   }
-// }
-
-// this.printXY = function() {
-//   if (mouseIsPressed) {
-//     console.log(mouseX, mouseY)
-//   }
-// }
--- game.js
+++ game.js
@@ -11,9 +11,6 @@ function draw() {
   background(0)
   frameRate(60)
   ghost.render()
-  this.fire()
-  this.updateBullet
-  this.printXY()
 
   text("X: " + mouseX, 0, HEIGHT / 4)
   text("Y: " + mouseY, 0, HEIGHT / 2)
@@ -21,8 +18,3 @@ function draw() {
   slugs.forEach(el => el.draw())
   slugs.forEach(el => el.update())
 }
-
-// for (let i = 0; i < slugs.length; i++) {
-//   slugs[i].render()
-//   slugs[i].update()
-// }
