2c228c28072131c7f649bcf1fcbd0ab82d1cab12
amrwski <cmf2071@gmail.com>
2019-04-03 13:50:59 +0200

    implemented bullet collision

--- scripts/bull-coll.js
+++ scripts/bull-coll.js
@@ -1,34 +1,31 @@
-let hit = false
-function draw() {
-  hit = collideCircleCircle(
-    replicants.forEach(
-      function(replic) {
-        replic.randPositionX, replic.randPositionY, replic.width
-      },
-      slugs.forEach(function(slug) {
-        slug.bullX, slug.bullY, 5
-      })
-    )
-  )
-  print("colliding? " + hit)
-}
-
-var hit = false
-function draw() {
-  background(255)
-  ellipse(200, 200, 100, 100)
-  ellipse(mouseX, mouseY, 150, 150)
-
-  hit = collideCircleCircle(mouseX, mouseY, 150, 200, 200, 100)
-
-  print("colliding? " + hit)
+// let hit = false
+// function draw() {
+//   replicCoordinates = replicants.map(function(replic) {
+//     return replic.randPositionX, replic.randPositionY, replic.width
+//   })
+//   bulletCoordinates = slugs.map(function(slug) {
+//     return slug.bullX, slug.bullY, 5
+//   })
+//   console.log(bulletCoordinates)
+//   hit = collideCircleCircle(replicCoordinates, bulletCoordinates)
+//   print("colliding? " + hit)
 // }
 
-// replicant
+// var hit = false
+// function draw() {
+//   background(255)
+//   ellipse(200, 200, 100, 100)
+//   ellipse(mouseX, mouseY, 150, 150)
+
+//   hit = collideCircleCircle(mouseX, mouseY, 150, 200, 200, 100)
+
+//   print("colliding? " + hit)
+// }
+
+// // replicant
 // fill(255, 69, 0)
 // ellipse(this.randPositionX, this.randPositionY, this.width, this.height)
 
-// bullet
+// // bullet
 // fill(200, 0, 200)
 // ellipse(this.bullX, this.bullY, 5)
-
--- scripts/game.js
+++ scripts/game.js
@@ -1,6 +1,7 @@
 let ghost
 let replicants = []
 let slugs = []
+let hit = false
 
 function setup() {
   mapFloor = loadImage("../assets/background/carbon-floor.png")
@@ -15,6 +16,20 @@ function draw() {
   slugs.forEach(el => el.draw())
   slugs.forEach(el => el.update())
   replicants.forEach(rep => rep.render())
+
+  // bullet collision
+  replicants.forEach(function(replic) {
+    const replicPosX = replic.randPositionX
+    const replicPosY = replic.randPositionY
+    const width = replic.width
+    slugs.forEach(function(slug) {
+      const slugPosX = slug.bullX
+      const slugPosY = slug.bullY
+      const radius = 5
+      hit = collideCircleCircle(replicPosX, replicPosY, width, slugPosX, slugPosY, radius)
+      print("colliding? " + hit)
+    })
+  })
 }
 
 setInterval(() => {
